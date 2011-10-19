bbde91a98482a659a079c7faa744b3c69e295d33
Eric Willisson <ericwillisson@gmail.com>
2011-10-19 13:03:38 -0400

    Forgot about existence of optional arguments. Going back to write those in.

--- relation-map.rkt
+++ relation-map.rkt
@@ -5,20 +5,9 @@
 
 (struct node (name shape))
 
+(struct edge (node1 node2 color style dir))
+
 (struct section (name (nodes #:mutable) (edges #:mutable)))
-
-(define new-node
-  (case-lambda
-    ((name) (new-node name 'box (first (graph))))
-    ((name shape) (new-node name shape (first (graph))))
-    ((name shape place)
-     (set-section-nodes! place (cons (node name shape)
-				     (section-nodes place))))))
-
-(define (new-section name)
-  (graph (cons (section name empty empty) (graph))))
-
-(define graph (make-parameter (list (section null empty empty))))
 
 (define (get-none) (last (graph)))
 (define-syntax none
@@ -26,23 +15,80 @@
 		   ((none a ...) ((get-none) a ...))
 		   (none (get-none))))
 
+(define new-node
+  (case-lambda
+    ((name shape) (new-node name shape (first (graph))))
+    ((name shape place)
+     (set-section-nodes! place (cons (node name shape)
+				     (section-nodes place))))))
+
+(define new-edge 
+  (case-lambda
+    ((node1 node2 color style) (new-edge node1 node2 color style null))
+    ((node1 node2 color style dir)
+     (set-section-edges! none (cons (edge (get-node node1) (get-node node2)
+					  color style (if (eq? dir 'null) null
+							  dir))
+				    (section-edges none))))))
+
+(define (new-section name)
+  (graph (cons (section name empty empty) (graph))))
+
+(define (new-graph) 
+  (list (section null empty empty)))
+
+(define graph (make-parameter (new-graph)))
+
+(define (all-nodes)
+  (define (iter node-list the-graph)
+    (if (empty? the-graph) node-list
+	(iter (append (section-nodes (first the-graph)) node-list)
+	      (rest the-graph))))
+  (iter empty (graph)))
+
+(define (get-node name)
+  (first
+   (filter (lambda (a-node) (string=? name (node-name a-node))) (all-nodes))))
+
 (define (safe-string a-string)
   (regexp-replace* "[^a-zA-Z0-9_]" a-string "_"))
 
 (define (output-graph)
   (display "graph G {\n")
   (indent
-   (for ((a-section (reverse (graph))))
+   (indented "rankdir = \"LR\";")
+   (for ((a-section (graph)))
 	(output-section a-section)))
   (display "}\n"))
 
+(define (url identifier)
+  (format "nodes/~a.html" identifier))
+
+(define (node-representation a-node)
+  (format "\"~a\"" (node-name a-node)))
+
 (define (output-node a-node)
-  (indented (format "\"~a\" [shape=~a];" (node-name a-node)
-		    (node-shape a-node))))
+  (indented (format "~a [shape=~a, URL=\"~a\"];" (node-representation a-node)
+		    (node-shape a-node)
+		    (url (safe-string (node-name a-node))))))
+
+(define (dir-string-if-present dir-string)
+  (if (not (null? dir-string))
+      (format ", dir=~a" dir-string)
+      ""))
+
+(define (output-edge an-edge)
+  (indented (format "~a -- ~a [color=~a, style=~a~a];"
+		    (node-representation (edge-node1 an-edge))
+		    (node-representation (edge-node2 an-edge))
+		    (edge-color an-edge) (edge-style an-edge)
+		    (dir-string-if-present (edge-dir an-edge)))))
 
 (define (handle-section a-section)
   (for ((a-node (reverse (section-nodes a-section))))
-       (output-node a-node)))
+       (output-node a-node))
+  (for ((an-edge (reverse (section-edges a-section))))
+       (output-edge an-edge)))
 
 (define (output-section a-section)
   (if (not (null? (section-name a-section)))
@@ -62,12 +108,22 @@
       ((name) (new-node name 'shape))
       ((name place) (new-node name 'shape place)))))
 
+(define-syntax edge-type
+  (syntax-rules ()
+    ((edge-type new-type color style) (edge-type new-type color style null))
+    ((edge-type new-type color style dir)
+     (define (new-type node1 node2)
+       (new-edge node1 node2 'color 'style 'dir)))))
+
 (define (example)
   (parameterize ((graph (list (section null empty empty))))
     (node-type character box)
     (node-type organization octagon)
+    (edge-type knows black solid forward)
+
     (new-section "All Pathwalkers")
     (character "Ged")
     (organization "Black Star Guild")
     (character "Iawen" none)
+    (knows "Ged" "Iawen")
     (output-graph)))
