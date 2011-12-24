d5607d31f181de7dff62c765366b4dfa19438453
Eric Willisson <ericwillisson@gmail.com>
2011-12-23 19:01:47 -0500

    Discovered some more useful functions, working on writing correct Unix-philosphy functions

--- relation-map.rkt
+++ relation-map.rkt
@@ -1,12 +1,16 @@
 #lang racket/base
 
 (require xml
-	 racket/contract)
+	 racket/contract
+	 racket/file)
 
 (require (prefix-in write-dot-file: "write-dot-file.rkt"))
 
-(define (render-dot-file dot-file)
-  (system (format "dot -Tcmapx -omap.html -Tpng -o"${input}map.png" $input"
+(define (render-dot-file dot-path)
+  (system (format "dot -Tcmapx -o~a -Tpng -o~a ~a"
+		  (path->string (path-replace-suffix dot-path "map.html"))
+		  (path->string (path-replace-suffix dot-path "map.png"))
+		  dot-path)))
 
 (define (main)
   (parameterize ((current-output-port (open-output-string)))
